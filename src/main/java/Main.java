67ec3b6947680d8f2608db1b94099be02387df7c
Joseph Victor <imjosephvictor@gmail.com>
2024-06-09 19:07:18 +0530

    multiple client connections possible

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,52 +1,61 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Main {
   public static void main(String[] args) throws IOException{
 
-       ServerSocket serverSocket = null;
-       Socket clientSocket = null;
-       BufferedReader in = null;
-       BufferedWriter out = null;
+    Selector selector = Selector.open();
+    ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+    serverSocketChannel.bind(new InetSocketAddress("localhost", 6379));
+    serverSocketChannel.configureBlocking(false);
+    serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
-       int port = 6379;
+    System.out.println("***Server started***");
 
-       try {
-        System.out.println("**Server started**");
-         serverSocket = new ServerSocket(port);
-         // Since the tester restarts your program quite often, setting SO_REUSEADDR
-         // ensures that we don't run into 'Address already in use' errors
-         serverSocket.setReuseAddress(true);
-         // Wait for connection from client.
-         clientSocket = serverSocket.accept();
-         System.out.println("Connection made: "+clientSocket.getInetAddress());
+    while(true){
+      int conns = selector.select();
+      if(conns == 0)  continue;
 
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-        System.out.println("in and out created");
+      Set<SelectionKey> selectedKeys = selector.selectedKeys();
+      Iterator<SelectionKey> iterator = selectedKeys.iterator();
 
-        while(true){
-          String cmd = in.readLine();
-          System.out.println(cmd);
-          if(cmd == null) break;
-          if(cmd.equalsIgnoreCase("PING"))
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+      while(iterator.hasNext()){
+        SelectionKey key = iterator.next();
+        
+        if(key.isAcceptable()){
+          ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
+          SocketChannel clientChannel = serverChannel.accept();
+          clientChannel.configureBlocking(false);
+          clientChannel.register(selector, SelectionKey.OP_READ);
+          System.out.println("Connection accepted: "+ clientChannel.getRemoteAddress());
+        }else if(key.isReadable()){
+          SocketChannel clientChannel = (SocketChannel) key.channel();
+          ByteBuffer buffer = ByteBuffer.allocate(256);
+          int byteRead = clientChannel.read(buffer);
+          if(byteRead == -1){
+            clientChannel.close();
+          }else{
+            buffer.flip();
+            String message = new String(buffer.array()).trim();
+            String response = "+PONG\r\n";
+            if(message.contains("PING")){
+              buffer.clear();
+              buffer.put(response.getBytes());
+              buffer.flip();
+              clientChannel.write(buffer);
+            }
+            buffer.clear();
+          }
         }
-
-       } catch (IOException e) {
-         System.out.println("IOException: " + e.getMessage());
-       } finally{
-        in.close();
-        out.close(); 
-        clientSocket.close();
-        serverSocket.close();
-       }
+        iterator.remove();
+      }
+    }
   }
 }
