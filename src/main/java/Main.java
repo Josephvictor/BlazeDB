169b41ca3041b59046f6b2e5606a755d0c6ac752
Joseph Victor <imjosephvictor@gmail.com>
2024-06-10 08:36:50 +0530

    test message output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -32,18 +32,23 @@ public class Main {
         if(key.isAcceptable()){
           ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
           SocketChannel clientChannel = serverChannel.accept();
+          
           clientChannel.configureBlocking(false);
           clientChannel.register(selector, SelectionKey.OP_READ);
           System.out.println("Connection accepted: "+ clientChannel.getRemoteAddress());
+
         }else if(key.isReadable()){
           SocketChannel clientChannel = (SocketChannel) key.channel();
           ByteBuffer buffer = ByteBuffer.allocate(256);
           int byteRead = clientChannel.read(buffer);
+          
           if(byteRead == -1){
             clientChannel.close();
           }else{
             buffer.flip();
             String message = new String(buffer.array()).trim();
+            System.out.println("[Received message: ]"+message);
+
             String response = "+PONG\r\n";
             if(message.contains("PING")){
               buffer.clear();
