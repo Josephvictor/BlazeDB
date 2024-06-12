3966de4fa6233a2bfce3611529e70edf129fd10f
Joseph Victor <imjosephvictor@gmail.com>
2024-06-12 12:18:00 +0530

    Receive handshake by master II 6

--- src/main/java/RequestParser.java
+++ src/main/java/RequestParser.java
@@ -35,7 +35,7 @@ public class RequestParser {
         }else if(parts[index].charAt(0) == '+'){
             parsedElements.add(parts[index].substring(1));
             
-            for(int i = 1; i <= parts.length; i++){
+            for(int i = 1; i < parts.length; i++){
                 parsedElements.add(parts[i]);
                 if(i==2)    break;
             }
